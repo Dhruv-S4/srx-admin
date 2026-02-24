d7fdd9e81e4def02cc468d43e074a2ac217d7e54
Dhruv Sharma <dhruv.sharma1@shiprocket.com>
2026-02-24 10:53:48 +0530

    Add sidebar and theme services; update toast styles
    - Introduced SidebarService and ThemeService for managing sidebar state and theme preferences.
    - Updated app component to initialize sidebar and theme settings on load.
    - Modified toast component styles for improved visual consistency.

--- src/app/app.ts
+++ src/app/app.ts
@@ -3,6 +3,8 @@ import { RouterOutlet } from '@angular/router';
 import { NgxUiLoaderModule } from 'ngx-ui-loader';
 import { Toast } from './shared/components/toast/toast';
 import { ToastService } from './shared/services/toast.service';
+import { SidebarService } from './shared/services/sidebar.service';
+import { ThemeService } from './shared/services/theme.service';
 
 @Component({
   selector: 'app-root',
@@ -13,8 +15,12 @@ import { ToastService } from './shared/services/toast.service';
 export class App {
   protected readonly title = signal('srx-admin');
   protected readonly toast = inject(ToastService);
-
+  protected readonly sidebarService = inject(SidebarService);
+  protected readonly themeService = inject(ThemeService);
+  
   constructor() {
+    this.sidebarService.setMobileView(window.innerWidth < 768);
+    this.themeService.setTheme(localStorage.getItem('theme') as 'light' | 'dark');
     this.toast.success('Hello, world!');
     setTimeout(() => {
     this.toast.success('Hello, world2!');
